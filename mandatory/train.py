25b410f24c1ac7d922399d8481027067e3ceab8e
Sylvain Maitre <smaitre@student.42.fr>
2026-04-15 22:53:46 +0200

    read dataset

--- mandatory/train.py
+++ mandatory/train.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 # Assisted-by: CLAUDE_SONNET:4.6_HIGH
 
+# Usage : ./train.py ../data/data.csv
+
 import argparse
 import sys
+
+# Read the dataset into a dict where the key is 'km' and the value is 'price'
+def read_dataset(dataset_path):
+    dataset = {}
+    with open(dataset_path, 'r') as f:
+        readline = f.readline()  # Skip the header
+        if readline.strip() != "km,price":
+            print("Warning: The first line of the dataset does not match the expected header 'km,price'.")
+            exit(1)
+        for line in f:
+            km, price = line.strip().split(',')
+            try:
+                km = int(km)
+                price = int(price)
+            except ValueError:
+                print(f"Warning: Skipping line with non-integer values: {line.strip()}")
+                continue
+            dataset[int(km)] = int(price)
+    return dataset
 
 # Main
 if __name__ == "__main__":
@@ -21,4 +42,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     dataset_path = args.dataset_path
+
+    # Read the dataset
+    dataset = read_dataset(dataset_path)
+    print(dataset)
 
